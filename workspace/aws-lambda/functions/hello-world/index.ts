d3b58aa826cbc30ae371f22799515dbbfbbc3e2e
junkor-1011 <tomoyukizxy@gmail.com>
2024-08-27 01:31:57 +0900

    aws lambda

---
+++ workspace/aws-lambda/functions/hello-world/index.ts
@@ -0,0 +1,42 @@
+import 'source-map-support/register';
+
+import { Logger } from '@aws-lambda-powertools/logger';
+import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
+import { MetricUnit, Metrics } from '@aws-lambda-powertools/metrics';
+import { logMetrics } from '@aws-lambda-powertools/metrics/middleware';
+import { Tracer } from '@aws-lambda-powertools/tracer';
+import { captureLambdaHandler } from '@aws-lambda-powertools/tracer/middleware';
+import middy from '@middy/core';
+import type { Context } from 'aws-lambda';
+
+const tracer = new Tracer();
+const logger = new Logger();
+const metrics = new Metrics();
+
+const lambdaHandler = async (
+  _event: unknown,
+  _context: Context,
+): Promise<void> => {
+  logger.debug('invoked.');
+
+  try {
+    const ipv4: string = await fetch('https://checkip.amazonaws.com/').then(
+      (res) => res.text(),
+    );
+
+    logger.info(`ipv4: ${ipv4}`);
+
+    metrics.addMetric('success', MetricUnit.Count, 1);
+  } catch (err) {
+    logger.error(`unexpected error occured: ${err}`);
+
+    metrics.addMetric('failed', MetricUnit.Count, 1);
+  }
+
+  logger.debug('finish');
+};
+
+export const handler = middy(lambdaHandler)
+  .use(captureLambdaHandler(tracer))
+  .use(injectLambdaContext(logger))
+  .use(logMetrics(metrics));
--- workspace/cdk-app/lib/workloads/app.constructs.ts
+++ workspace/cdk-app/lib/workloads/app.constructs.ts
@@ -1,11 +1,35 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import {
   Aws,
+  Duration,
   RemovalPolicy,
   aws_dynamodb as dynamodb,
   aws_lambda as lambda,
   aws_s3 as s3,
 } from 'aws-cdk-lib';
+import {
+  NodejsFunction,
+  type NodejsFunctionProps,
+  OutputFormat,
+} from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const esmBanner =
+  'import { createRequire as topLevelCreateRequire } from "node:module"; import url from "node:url"; const require = topLevelCreateRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));';
+
+const lambdaNodejsBundlingOption = {
+  sourceMap: false,
+  minify: true,
+  format: OutputFormat.ESM,
+  tsconfig: path.join(__dirname, '../../../aws-lambda/tsconfig.json'),
+  banner: esmBanner,
+  externalModules: ['@aws-sdk/*'],
+} as const satisfies NodejsFunctionProps['bundling'];
 
 export class SampleApp extends Construct {
   constructor(scope: Construct, id: string) {
@@ -14,6 +38,25 @@ export class SampleApp extends Construct {
     new s3.Bucket(this, 'dummy output bucket', {
       bucketName: `dummy-output-bucket-${Aws.ACCOUNT_ID}-${Aws.REGION}`,
       removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    new NodejsFunction(this, 'hello-world-function', {
+      functionName: 'LF-hello-world',
+      handler: 'handler',
+      entry: path.join(
+        __dirname,
+        '../../../aws-lambda/functions/hello-world/index.ts',
+      ),
+      runtime: lambda.Runtime.NODEJS_20_X,
+      bundling: lambdaNodejsBundlingOption,
+      environment: {
+        POWERTOOLS_LOG_LEVEL: 'DEBUG',
+        POWERTOOLS_SERVICE_NAME: 'hello-world-function',
+      },
+      tracing: lambda.Tracing.ACTIVE,
+      architecture: lambda.Architecture.ARM_64,
+      timeout: Duration.seconds(10),
+      memorySize: 256,
     });
   }
 }
